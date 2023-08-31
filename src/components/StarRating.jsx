9322f3490a92c9f3201b23671639cd65f1d50b2e
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-08-31 18:54:43 +0300

    Enhance Stars Component

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 import Star from './Star';
 
 const containerStyle = {
@@ -12,25 +13,38 @@ const starContainerStyle = {
   gap: '4px',
 };
 
-const textStyle = {
-  lineHeight: '1',
-  margin: '0',
-};
-
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating = 5,
+  color = '#fcc419',
+  size = 48,
+  className = '',
+  message = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  const [rating, setRating] = useState(
+    defaultRating <= maxRating ? defaultRating : 0
+  );
   const [tempRating, setTempRating] = useState(0);
 
   const handleRating = (rating) => {
     setRating(rating);
+    onSetRating(rating);
   };
 
   const handleTempRating = (rating) => {
     setTempRating(rating);
   };
 
+  const textStyle = {
+    lineHeight: '1',
+    margin: '0',
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
@@ -39,11 +53,27 @@ const StarRating = ({ maxRating = 5 }) => {
             onRate={() => handleRating(i + 1)}
             onHoverIn={() => handleTempRating(i + 1)}
             onHoverOut={() => handleTempRating(0)}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ''}</p>
+      <p style={textStyle}>
+        {message.length === maxRating
+          ? message[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ''}
+      </p>
     </div>
   );
 };
 export default StarRating;
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  className: PropTypes.string,
+  message: PropTypes.array,
+  defaultRating: PropTypes.number,
+  onSetRatingL: PropTypes.func,
+};
